82891afe3a8fc720edd3384cdf7fa7e17c0df25e
TsAlexandr <lunoface@mail.ru>
2023-01-09 23:33:28 +0700

    fix order by for blog name, add substring

--- src/features/public/posts/posts.repository.ts
+++ src/features/public/posts/posts.repository.ts
@@ -14,7 +14,7 @@ export class PostsRepository {
   ): Promise<Paginator<PostsCon[]>> {
     let dynamicSort = `p."${sortBy}"`;
     if ((sortBy = 'blogName')) {
-      dynamicSort = `substring(name, 9)::int `;
+      dynamicSort = `name COLLATE "C"`;
     }
     const query = await this.dataSource.query(
       `
