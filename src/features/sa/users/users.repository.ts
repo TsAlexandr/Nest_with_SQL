f0822c71c0c42477558c5cdd3885522add5538d0
TsAlexander <lunoface@mail.ru>
2023-02-08 04:10:29 +0700

    add delete from quiz

--- src/features/sa/users/users.repository.ts
+++ src/features/sa/users/users.repository.ts
@@ -298,6 +298,7 @@ export class UsersRepository {
     DELETE FROM public.comments;
     DELETE FROM public."userBlackList";
     DELETE FROM public."questions" CASCADE;
+    DELETE FROM public.game CASCADE;
     `,
     );
   }
