19c3a431621ad0405e724e0a0073ef5ce1fa303f
TsAlexander <lunoface@mail.ru>
2023-02-08 16:51:36 +0700

    fix sort in get questions for current game

--- src/quiz/quiz.repository.ts
+++ src/quiz/quiz.repository.ts
@@ -339,7 +339,7 @@ export class QuizRepository {
     LEFT JOIN public.answers a
     ON p."questionsId" = a."questionId"
     WHERE g.id = $1
-    ORDER BY q."createdAt" ASC
+    ORDER BY q."createdAt" DESC
     `,
       [id],
     );
