30b8db8c0cb489b0c32a384c6895c755523f6753
TsAlexander <lunoface@mail.ru>
2023-02-10 01:10:44 +0700

    fix score in finished game

--- src/quiz/quiz-pair/usecases/getAllGames.ts
+++ src/quiz/quiz-pair/usecases/getAllGames.ts
@@ -13,6 +13,27 @@ export class GetAllGames {
 export class GetAllGamesHandler implements IQueryHandler<GetAllGames> {
   constructor(private quizRepo: QuizRepository) {}
   async execute(query: GetAllGames): Promise<any> {
-    return this.quizRepo.findAllGames(query.query, query.userId);
+    const games = await this.quizRepo.findAllGames(query.query, query.userId);
+    games.items.map((el) => {
+      if (el.firstPlayerProgress.answers.length > 4) {
+        const answers1 = el.firstPlayerProgress.answers;
+        const answers2 = el.secondPlayerProgress.answers;
+        if (
+          el.status == 'Finished' &&
+          answers1[4].addedAt < answers2[4].addedAt &&
+          answers1.map((el) => el.answer == 'Correct').length > 0
+        ) {
+          el.firstPlayerProgress.score++;
+        } else if (
+          el.status == 'Finished' &&
+          answers2[4].addedAt < answers1[4].addedAt &&
+          answers2.map((el) => el.answer == 'Correct').length > 0
+        ) {
+          el.secondPlayerProgress.score++;
+        }
+      }
+    });
+
+    return games;
   }
 }
